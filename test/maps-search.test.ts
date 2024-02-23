476330373970f477f40e940c3e6c84044ccdac95
Brian Ng <brianngchongeu@gmail.com>
2024-02-23 16:03:00 +1100

    feat: first commit

---
+++ test/maps-search.test.ts
@@ -0,0 +1,59 @@
+import { config } from "dotenv";
+import { describe } from "@jest/globals";
+import { getPlaceAutocomplete } from "../src/maps-api";
+import { getAutoCompleteDetails } from "../src";
+
+config();
+
+// Declare TomTom API Key
+const apiKey = process.env.TOMTOM_API_KEY ?? "";
+
+// These are end-to-end tests and need an api key
+describe("Tomtom Places E2E Tests", () => {
+  describe("getAutoCompleteDetails", () => {
+    it("returns a promise", () => {
+      const res = getAutoCompleteDetails("Charlotte Street");
+      expect(res).toBeInstanceOf(Promise);
+    });
+
+    it("can fetch from the autocomplete api", async () => {
+      const res = await getAutoCompleteDetails("Charlotte Street");
+      const firstRes = res[0].address; // TODO don't forget `address` prop is accessed in first key item of array of objects..
+      //expect(firstRes).toHaveProperty("placeId"); // TODO Error: no such property in `address` prop
+      //expect(firstRes).toHaveProperty("streetNumber");  // TODO Error: no such property in `address` prop
+      expect(firstRes).toHaveProperty("countryCode");
+      expect(firstRes).toHaveProperty("country");
+      expect(firstRes).toHaveProperty("freeformAddress");
+      expect(firstRes).toHaveProperty("municipality");
+    });
+  });
+
+  describe("getAutoCompleteDetails", () => {
+    it("returns a promise", () => {
+      const res = getAutoCompleteDetails("Charlotte Street");
+      expect(res).toBeInstanceOf(Promise);
+    });
+
+    it("can fetch from the autocomplete api", async () => {
+      const res = await getAutoCompleteDetails("Charlotte Street");
+      const firstRes = res[0].address; // TODO don't forget `address` prop is accessed in first key item of array of objects..
+      //expect(firstRes).toHaveProperty("placeId"); // TODO Error: no such property in `address` prop
+      //expect(firstRes).toHaveProperty("streetNumber");  // TODO Error: no such property in `address` prop
+      expect(firstRes).toHaveProperty("countryCode");
+      expect(firstRes).toHaveProperty("country");
+      expect(firstRes).toHaveProperty("freeformAddress");
+      expect(firstRes).toHaveProperty("municipality");
+    });
+  });
+
+  describe("getPlaceAutocomplete", () => {
+    it("handles no results", async () => {
+      const res = await getPlaceAutocomplete(apiKey, "asfasffasfasafsafs");
+      expect(res).toStrictEqual([]);
+    });
+
+    it("handles error", async () => {
+      expect(getPlaceAutocomplete(apiKey, "")).rejects.toThrow();
+    });
+  });
+});
