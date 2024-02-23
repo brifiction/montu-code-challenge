57cf03880f24db0b0a9dbf7560e189b2b82b3361
Brian Ng <brianngchongeu@gmail.com>
2024-02-23 17:21:04 +1100

    feat: extend tomtom autocomplete fuzzy search feature
    - allow fuzzy search performance by utilising countrySet filter
    - using a new strict enum list of country codes, for countrySet filter

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import { getPlaceAutocomplete } from "./maps-api";
+import { COUNTRY_ISO_CODES } from "./utils/constants";
 
 export async function getAutoCompleteDetails(address: any): Promise<any> {
   const apiKey = process.env.TOMTOM_API_KEY;
+  const countrySet = COUNTRY_ISO_CODES.AUS; // TODO This could be an environment var, func param etc
+
   // get autocomplete results
-  const res = getPlaceAutocomplete(apiKey, address).then(
-    async (autocompleteResults) => {
-      return autocompleteResults;
-    }
+  const res = getPlaceAutocomplete(apiKey, address, countrySet).then(
+    (autocompleteResults) => autocompleteResults
   );
   // loop over and get details and map results
   return res;
--- src/maps-api.ts
+++ src/maps-api.ts
@@ -1,17 +1,22 @@
 import axios from "axios";
+import { COUNTRY_ISO_CODES } from "./utils/constants";
 
 // https://developer.tomtom.com/search-api/documentation/search-service/fuzzy-search
-export async function getPlaceAutocomplete(key: string, address: string) {
+export async function getPlaceAutocomplete(
+  key: string | undefined,
+  address: string,
+  countrySet: COUNTRY_ISO_CODES = COUNTRY_ISO_CODES.AUS
+) {
   const autocomplete = await axios.get(
     `https://api.tomtom.com/search/2/search/${address}.json'`,
     {
       params: {
         key,
         limit: 100,
+        countrySet,
       },
     }
   );
-  return autocomplete.data.results.map((result) => {
-    return result;
-  });
+
+  return autocomplete.data.results;
 }
